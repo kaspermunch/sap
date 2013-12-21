ab1b0768d4aed47096254c6ff5c53375be4e558a
Kasper Munch <kaspermunch@birc.au.dk>
2013-12-21 22:01:28 +0100

    windows comopilation fix

--- ext/barcoder/crossplatform.h
+++ ext/barcoder/crossplatform.h
@@ -17,22 +17,16 @@
 #endif
 
 
+
 /* In your header files, wherever you want an interface or API made public outside the current DSO, place __attribute__ ((visibility ("default"))) in struct, class and function declarations you wish to make public (it's easier if you define a macro as this). You don't need to specify it in the definition. Then, alter your make system to pass -fvisibility=hidden to each call of GCC compiling a source file. If you are throwing exceptions across shared object boundaries see the section "Problems with C++ exceptions" below. Use nm -C -D on the outputted DSO to compare before and after to see the difference it makes.
  */
 
-#if defined _WIN32 || defined __CYGWIN__
-  #ifdef BUILDING_DLL
-    #ifdef __GNUC__
-      #define DLL_PUBLIC __attribute__ ((dllexport))
-    #else
-      #define DLL_PUBLIC __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
-    #endif
+
+defined _WIN32 || defined __CYGWIN__
+  #ifdef __GNUC__
+     #define DLL_PUBLIC __attribute__ ((dllexport))
   #else
-    #ifdef __GNUC__
-      #define DLL_PUBLIC __attribute__ ((dllimport))
-    #else
-      #define DLL_PUBLIC __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
-    #endif
+     #define DLL_PUBLIC __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
   #endif
   #define DLL_LOCAL
 #else
@@ -44,6 +38,32 @@
     #define DLL_LOCAL
   #endif
 #endif
+
+/* #if defined _WIN32 || defined __CYGWIN__
+ *   #ifdef BUILDING_DLL
+ *     #ifdef __GNUC__
+ *       #define DLL_PUBLIC __attribute__ ((dllexport))
+ *     #else
+ *       #define DLL_PUBLIC __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
+ *     #endif
+ *   #else
+ *     #ifdef __GNUC__
+ *       #define DLL_PUBLIC __attribute__ ((dllimport))
+ *     #else
+ *       #define DLL_PUBLIC __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
+ *     #endif
+ *   #endif
+ *   #define DLL_LOCAL
+ * #else
+ *   #if __GNUC__ >= 4
+ *     #define DLL_PUBLIC __attribute__ ((visibility ("default")))
+ *     #define DLL_LOCAL  __attribute__ ((visibility ("hidden")))
+ *   #else
+ *     #define DLL_PUBLIC
+ *     #define DLL_LOCAL
+ *   #endif
+ * #endif
+ */
 
 /* extern "C" DLL_PUBLIC void function(int a);
  * class DLL_PUBLIC SomeClass
